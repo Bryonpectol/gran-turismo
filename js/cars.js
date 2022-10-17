1b0607c19747abfd2ac5af1a1ae10c9867a2f50b
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2022-10-17 00:10:58 -0600

    Update cars.js

--- js/cars.js
+++ js/cars.js
@@ -22,12 +22,15 @@ allCars.forEach(myCar => {
 
 let makE = document.createElement("p");
 let modeL = document.createElement("p");
+let imG = document.createElement("img");
 
-makE.textContent = "Make" + myCar.Make;
-modeL.textcontent = "Model" + myCar.Model;
+makE.textContent = "Make: " + myCar.Make;
+modeL.textcontent = "Model: " + myCar.Model;
+imG.setAttribute("src", myCar.image);
 
 mainContainer.appendChild(makE);
 makE.appendChild(modeL);
+makE appendChild(imG);
 
 });
 
