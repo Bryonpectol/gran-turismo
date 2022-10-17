7e07230980947ff05174b5ac94f7cc3a17660968
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2022-10-16 23:53:09 -0600

    Update cars.js

--- js/cars.js
+++ js/cars.js
@@ -1,3 +1,34 @@
-import { cars } from '../js/cardata.js';
+import { cars } from '../js/carsdata.js';
 
+const mainContainer = document.getElementById("container");
 
+const allCars = cars.map(myCar => {
+  let carsYeah = cars.find(CAR => {
+    return CAR.name === myCar.name;
+  });
+
+  return {
+    // this is how the script finds the item in the array.
+    Model: myCar.model,
+    Make: carsYeah.make,
+    Id: carsYeah.id,
+    image: carsYeah.img,
+    PP: carsYeah.pp,
+    DT: carsYeah.drivetrain,
+  };
+});
+
+allCars.forEach(my car => {
+
+let makE = document.createElement("p");
+let modeL = document.createElement("p");
+
+makE.textContent = myCar.Make;
+modeL.textcontent = myCar.Model;
+
+mainContainer.appendChild(makE);
+mainContainer.appendChild(modeL);
+
+});
+
+document.body.appendChild(mainContainer);
